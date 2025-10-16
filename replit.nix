797267ff2ea3571590f64e2f8b275b77e5a86c4b
mandszinha <mandyjacksxn@gmail.com>
2025-10-16 20:46:33 -0300

    replit.nix

--- replit.nix
+++ replit.nix
@@ -1,1 +1,25 @@
+{ pkgs }: 
+{
+  deps = [
+    pkgs.python311Full
+    pkgs.nodejs
+    pkgs.curl
+    pkgs.gcc
+    pkgs.glib
+    pkgs.glibc
+    pkgs.libX11
+    pkgs.libXcomposite
+    pkgs.libXdamage
+    pkgs.libXfixes
+    pkgs.libgbm
+    pkgs.libxkbcommon
+    pkgs.alsaLib
+    pkgs.nss
+    pkgs.nspr
+    pkgs.dbus
+    pkgs.atk
+    pkgs.atkmm
+    pkgs.atspi2
+  ];
+}
 
